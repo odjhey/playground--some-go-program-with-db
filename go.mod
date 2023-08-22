bf025334cc79196beab2f91a38898792d5b1499c
odjhey <odjhey@gmail.com>
2023-08-22 10:55:16 +0800

    feat: add /message

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/halakata/go-pokemon-api
 
 go 1.20
 
-require github.com/go-chi/chi/v5 v5.0.10 // indirect
+require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-chi/render v1.0.3 // indirect
+)
--- main.go
+++ main.go
@@ -7,7 +7,38 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
+
+type SomeMessage struct {
+	ID      int    `json:"id"`
+	Message string `json:"message"`
+}
+
+func (s *SomeMessage) Bind(r *http.Request) error {
+	if s.Message == "" {
+		return errors.New("message is required")
+	}
+
+	return nil
+}
+
+func (s *SomeMessage) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+type ErrResponse struct {
+	Err            error  `json:"-"`
+	HttpStatusCode int    `json:"-"`
+	StatusText     string `json:"status"`
+	AppCode        int64  `json:"code,omitempty"`
+	ErrorText      string `json:"error,omitempty"`
+}
+
+func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	render.Status(r, e.HttpStatusCode)
+	return nil
+}
 
 type Handler func(w http.ResponseWriter, r *http.Request) error
 
@@ -23,15 +54,48 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world"))
 	})
 
 	r.Method("GET", "/x", Handler(someHandler))
+	r.Method("GET", "/message", Handler(getMessage))
+	r.Method("POST", "/message", Handler(createMessage))
 
 	http.ListenAndServe(":8080", r)
 
+}
+
+func createMessage(w http.ResponseWriter, r *http.Request) error {
+
+	message := &SomeMessage{}
+
+	if err := render.Bind(r, message); err != nil {
+		render.Render(w, r, &ErrResponse{
+			Err:            err,
+			HttpStatusCode: 500,
+			StatusText:     "something wrong haha",
+			ErrorText:      err.Error()})
+		return nil
+	}
+
+	render.Render(w, r, message)
+
+	return nil
+
+}
+
+func getMessage(w http.ResponseWriter, r *http.Request) error {
+	message := &SomeMessage{
+		ID:      12,
+		Message: "hellow world",
+	}
+
+	render.Render(w, r, message)
+
+	return nil
 }
 
 func someHandler(w http.ResponseWriter, r *http.Request) error {
